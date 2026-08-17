b116519b0678e78369d44889f007d586b088866f
twd <viraj@thewebdecor.com>
2026-08-17 17:14:46 +0530

    day 3 test 10

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -6,6 +6,10 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 const app = getApps().length
   ? getApps()[0]
-  : initializeApp({ credential: cert(serviceAccount) });
+  : initializeApp({
+        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      });
 
 export const adminDb = getFirestore(app);
